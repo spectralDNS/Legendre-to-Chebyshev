eb378b6be704fa9583acbe64e79713afa30b42d7
Mikael Mortensen <mikaem@math.uio.no>
2023-05-01 17:31:37 +0200

    Change repeat times

--- C/tests/test.c
+++ C/tests/test.c
@@ -86,11 +86,7 @@ int main(int argc, char *argv[]) {
   test_foreward_backward(10000, 36);
   test_speed(100, 10, 100, 0);
   for (size_t i = 4; i < 12; i++)
-    test_speed(36*pow(2, i+2), 36, 100, 0);
-
-  //test_speed(1000, 40, 500, 0);
-  //test_speed(10000, 50, 500, 1);
-  //test_speed(100000, 50, 500, 0);
+    test_speed(36*pow(2, i+2), 36, 10, 0);
   test_direct(10);
   test_direct(200);
   test_direct(1000);
