96db1ba7a2325b0c0e6e0d19093c770a071bcecf
Mikael Mortensen <mikaem@math.uio.no>
2023-05-03 15:42:01 +0200

    Fixing l2c bin

--- src/bin/l2c.c
+++ src/bin/l2c.c
@@ -7,21 +7,21 @@ int main(int argc, char *argv[])
     int opt;
     size_t N;
     size_t maxs = 36;
-    size_t v = 0;
+    size_t verbose = 0;
     double m = 0;
     size_t repeat = 1;
-    unsigned direction = 0;
+    unsigned direction;
     char *help = "Usage: ./l2c options\n"
                  "  -h      Show this message\n"
                  "  -N      Size of transform\n"
                  "  -s      Estimated max size of smallest submatrix  (optional, default=36)\n"
-                 "  -m      Data decay coefficient. \n"
+                 "  -m      Data decay coefficient (optional, default=0). \n"
                  "  -r      Repeat computation this many times (for timing, default=1)\n"
-                 "  -v      Level of verbosity\n"
+                 "  -v      Level of verbosity (optional, default=0)\n"
                  "  -d      Direction of transform. 0 for Legendre to Chebyshev,\n"
                  "          1 for Chebyshev to Legendre and 2 use both and check accuracy\n";
 
-    while ((opt = getopt(argc, argv, "N:d:s::m::r::v:h")) != -1)
+    while ((opt = getopt(argc, argv, ":N:d:s::m::r::v::h")) != -1)
     {
        switch (opt)
        {
@@ -33,27 +33,30 @@ int main(int argc, char *argv[])
           break;
         case 'm':
           m = atof(optarg);
+          break;
         case 'r':
           repeat = atoi(optarg);
           break;
         case 'd':
           direction = atoi(optarg);
           break;
+        case 'v':
+          verbose = atoi(optarg);
+          break;
         case 'h':
           puts(help);
           return 0;
           break;
         default:
-          printf("Exiting...");
+          printf("Wrong argument, exiting...");
           exit(-1);
        }
     }
-    fmm_plan* fmmplan = create_fmm(N, maxs, direction, v);
+    fmm_plan* fmmplan = create_fmm(N, maxs, direction, verbose);
     double* input_array=(double *)calloc(fmmplan->Nn, sizeof(double));
     double* output_array=(double *)calloc(fmmplan->Nn, sizeof(double));
     for (size_t i = 0; i < N; i++)
-      input_array[i] = 1.0/pow(i, m);
-
+      input_array[i] = 1.0/pow(i+1, m);
     struct timeval t0, t1;
     double min_time = 1e8;
     size_t flops;
@@ -71,7 +74,8 @@ int main(int argc, char *argv[])
         double s1 = tdiff_sec(t0, t1);
         for (size_t j = 0; j < N; j++)
             error += pow(input_array[j]-ia[j], 2);
-        printf("L2 Error back and forth %2.4e in %2.4e s\n", sqrt(error), s1);
+        if (verbose > 0)
+          printf("L2 Error back and forth %2.4e in %2.4e s\n", sqrt(error), s1);
         free(ia);
     }
     else
@@ -89,9 +93,12 @@ int main(int argc, char *argv[])
         }
         size_t eflops = (4.5*fmmplan->s + 4*fmmplan->M + (6+2.5)*fmmplan->M*fmmplan->M/fmmplan->s )*N;
         gettimeofday(&t1, 0);
-        printf("Timing %2.6e s average %2.6e min of %lu times %lu N %lu S %lu F\n", tdiff_sec(t0, t1)/repeat, min_time, repeat, fmmplan->Nn, get_number_of_submatrices(fmmplan->N, fmmplan->s, fmmplan->L), flops);
-        printf("Estimated flops %lu\n", eflops);
-        printf("Norm %2.8e\n", norm(output_array, N));
+        if (verbose > 0)
+        {
+          printf("Timing %2.6e s average %2.6e min of %lu times %lu N %lu S %lu F\n", tdiff_sec(t0, t1)/repeat, min_time, repeat, fmmplan->Nn, get_number_of_submatrices(fmmplan->N, fmmplan->s, fmmplan->L), flops);
+          printf("Estimated flops %lu\n", eflops);
+          printf("Norm %2.8e\n", norm(output_array, N));
+        }
 
         if (N < 10001)
         {
@@ -105,7 +112,8 @@ int main(int argc, char *argv[])
             }
             gettimeofday(&r1, 0);
             double s1 = tdiff_sec(r0, r1);
-            printf("Norm %2.8e time %2.4e F %lu \n", norm(od, N), s1/repeat, fd);
+            if (verbose > 0)
+              printf("Norm %2.8e time %2.4e F %lu \n", norm(od, N), s1/repeat, fd);
 
             double err = 0.0;
             double errinf = 0.0;
@@ -117,8 +125,11 @@ int main(int argc, char *argv[])
                 errinf = fmax(errinf, e0);
                 omax = fmax(omax, output_array[i]);
             }
-            printf("Error     %2.6e\n", sqrt(err));
-            printf("Error inf %2.8e\n", errinf/omax);
+            if (verbose > 0)
+            {
+              printf("Error     %2.6e\n", sqrt(err));
+              printf("Error inf %2.8e\n", errinf/omax);
+            }
             free(od);
         }
     }
