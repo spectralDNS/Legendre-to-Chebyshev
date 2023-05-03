40a2626e0fe90e6591892fa0880b6c9b59b9f020
Mikael Mortensen <mikaem@math.uio.no>
2023-05-03 10:07:06 +0200

    remove pytest

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,8 +1,8 @@
 import numpy as np
 import l2c
-import pytest
+#import pytest
 
-@pytest.mark.parametrize('N', (1000, 5000, 10000))
+#@pytest.mark.parametrize('N', (1000, 5000, 10000))
 def test_accuracy(N):
     u = np.ones(N)
     b = np.zeros_like(u)
@@ -12,3 +12,8 @@ def test_accuracy(N):
     c = C(b, c, 1)
     assert np.linalg.norm(c-1) < 1e-8
     del C
+
+if __name__ == '__main__':
+    test_accuracy(1000)
+    test_accuracy(5000)
+    test_accuracy(10000)
